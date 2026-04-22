62d246c70f75316d03824f3e68611db3dd1f271b
Shulgin Sergey <111587168+SerzhRedAlert@users.noreply.github.com>
2026-04-22 19:21:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,117 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int alpha = 26;
+int last = 0;
+
 struct node {
-    array<int, 26> nx;
-    int suf, pre;
+    array<int, alpha> nx;
+    int link, len, first_pos;
+    bool is_clone;
 
     node() {
         nx.fill(-1);
-        suf = -1, pre = -1;
+        link = -1, len = 0, first_pos = -1, is_clone = false;
     }
 };
 
 vector<node> s = {node()};
+vector<vector<int>> tree = {{}};
 
-int add(int a, const int x) {
-    const int b = int(s.size());
+int make_v() {
     s.emplace_back();
-    s[b].pre = a, s[b].suf = 0;
-    for (; a != -1; a = s[a].suf) {
-        if (s[a].nx[x] == -1) {
-            s[a].nx[x] = b;
-            continue;
-        }
-        int c = s[a].nx[x];
-        if (s[c].pre == a) {
-            s[b].suf = c;
-            break;
-        }
-        const int d = int(s.size());
-        s.emplace_back();
-        s[d].nx = s[c].nx;
-        s[d].pre = a, s[d].suf = s[c].suf;
-        s[b].suf = d, s[c].suf = d;
-        for (; a != -1 && s[a].nx[x] == c; a = s[a].suf) {
-            s[a].nx[x] = d;
-        }
-        break;
+    tree.emplace_back();
+    return int(s.size()) - 1;
+}
+
+void add(int pos, int c) {
+    int cur = make_v();
+    s[cur].len = s[last].len + 1;
+    s[cur].first_pos = pos;
+    int p = last;
+    while (p != -1 && s[p].nx[c] == -1) {
+        s[p].nx[c] = cur;
+        p = s[p].link;
     }
-    return b;
+    if (p == -1) {
+        s[cur].link = 0;
+    } else {
+        int q = s[p].nx[c];
+        if (s[p].len + 1 == s[q].len) {
+            s[cur].link = q;
+        } else {
+            int ns = make_v();
+            s[ns] = s[q], s[ns].len = s[p].len + 1;
+            s[ns].is_clone = true;
+            while (p != -1 && s[p].nx[c] == q) {
+                s[p].nx[c] = ns;
+                p = s[p].link;
+            }
+            s[q].link = s[cur].link = ns;
+        }
+    }
+    last = cur;
+}
+
+void build() {
+    for (int i = 1; i < int(s.size()); ++i) {
+        tree[s[i].link].emplace_back(i);
+    }
+}
+
+int walk(const string &p) {
+    int v = 0;
+    for (auto ch : p) {
+        int c = ch - 'a';
+        if (c < 0 || c >= alpha) {
+            return -1;
+        }
+        v = s[v].nx[c];
+        if (v == -1) {
+            return -1;
+        }
+    }
+    return v;
+}
+
+vector<int> find(const string &p) {
+    int v = walk(p);
+    if (v == -1) return {};
+    vector<int> res;
+    stack<int> st;
+    st.emplace(v);
+    while (!st.empty()) {
+        int u = st.top();
+        st.pop();
+        if (!s[u].is_clone) {
+            int start = s[u].first_pos - int(p.size()) + 1;
+            res.emplace_back(start);
+        }
+        for (auto to : tree[u]) {
+            st.emplace(to);
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
 }
 
 void solve() {
-    int last = 0;
-    char type;
-    while (cin >> type) {
-        if (type == '?') {
-            string cur;
-            cin >> cur;
-            int v = 0;
-            for (const char i : cur) {
-                const int c = (i > 'Z' ? i - 'a' : i - 'A');
-                v = s[v].nx[c];
-                if (v == -1) break;
-            }
-            cout << (v == -1 ? "NO\n" : "YES\n");
-        } else {
-            string cur;
-            cin >> cur;
-            for (const char i : cur) {
-                const int c = (i > 'Z' ? i - 'a' : i - 'A');
-                last = add(last, c);
-            }
-        }
+    string text;
+    cin >> text;
+    s.reserve(2 * text.size() + 1), tree.reserve(2 * text.size() + 1);
+    for (int i = 0; i < int(text.size()); ++i) {
+        add(i, text[i] - 'a');
+    }
+    build();
+    int q;
+    cin >> q;
+    while (q--) {
+        string cur;
+        cin >> cur;
+        auto res = find(cur);
+        cout << res.size() << " ";
+        for (auto i : res) cout << i + 1 << " ";
+        cout << "\n";
     }
 }
 
